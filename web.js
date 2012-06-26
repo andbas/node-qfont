f9d93be2ac52945e0dc1e2809d9140b66defdb98
Andrei Bas <bas.andrei@gmail.com>
2012-06-26 12:00:49 +0300

    web.js added, all history had to be cleaned, because it contains sensitive info

---
+++ web.js
@@ -0,0 +1,58 @@
+var express = require('express'),
+    app = express.createServer(express.logger()),
+    format = require('util').format,
+    fs = require('fs'),
+    dropbox  = require("dbox").app({"app_key": process.env['app_key'], "app_secret": process.env['app_secret'] }),
+    client = dropbox.createClient({oauth_token_secret: process.env['oauth_token_secret'], oauth_token: process.env['oauth_token'], uid: process.env['uid']});
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.set('view options', { layout: false });
+app.use(express.bodyParser());
+app.use(express.static(__dirname+'/public'));
+
+app.get('/', function(req, res) {
+  res.render('pindex', {
+    title: 'qfont - check your font quick'
+  });
+});
+
+app.get('/demos/:name', function(req, res) {
+  var name = req.params.name;
+  res.render('font-face', {
+    font: name
+  });
+});
+
+app.post('/fonts/add', function (req, res, next) {
+
+  var fileName = req.files['file-input'].name,
+      filePath = req.files['file-input'].path;
+
+  console.log('controller (POST): /fonts/add');
+  console.log('fileName='+fileName);
+  console.log('filePath='+filePath);
+
+  var inp = fs.createReadStream(filePath);
+
+  fs.readFile(filePath, function (err, data) {
+    if (err) throw err;
+    client.put(fileName, data, function(status, reply){
+      console.log(fileName+' have been saved');
+      res.redirect('/demos/'+fileName);
+    });
+  });
+});
+
+app.get('/fonts/:name', function(req, res){
+  var name = req.params.name;
+  client.get(name,function(status, reply, metadata){
+    res.send(reply);
+  });
+});
+
+
+var port = process.env['app_port'] || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
